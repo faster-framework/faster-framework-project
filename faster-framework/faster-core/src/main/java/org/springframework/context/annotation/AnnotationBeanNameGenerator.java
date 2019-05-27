1e171c2930d4439b718030670dcf5a9da99e4c5a
zhangbowen <515445681@qq.com>
2019-05-27 19:30:24 +0800

    mod: beanName生成器

--- faster-framework/faster-core/src/main/java/org/springframework/context/annotation/AnnotationBeanNameGenerator.java
+++ faster-framework/faster-core/src/main/java/org/springframework/context/annotation/AnnotationBeanNameGenerator.java
@@ -1,18 +1,82 @@
 package org.springframework.context.annotation;
 
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanNameGenerator;
-import org.springframework.util.Assert;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author zhangbowen
  */
 public class AnnotationBeanNameGenerator implements BeanNameGenerator {
+    private static final String COMPONENT_ANNOTATION_CLASSNAME = "org.springframework.stereotype.Component";
+
+
     @Override
     public String generateBeanName(BeanDefinition definition, BeanDefinitionRegistry registry) {
-        String beanClassName = definition.getBeanClassName();
-        Assert.state(beanClassName != null, "No bean class name set");
-        return beanClassName;
+        if (definition instanceof AnnotatedBeanDefinition) {
+            String beanName = determineBeanNameFromAnnotation((AnnotatedBeanDefinition) definition);
+            if (StringUtils.hasText(beanName)) {
+                // Explicit bean name found.
+                return beanName;
+            }
+        }
+        return definition.getBeanClassName();
     }
+
+    /**
+     * Derive a bean name from one of the annotations on the class.
+     *
+     * @param annotatedDef the annotation-aware bean definition
+     * @return the bean name, or {@code null} if none is found
+     */
+    protected String determineBeanNameFromAnnotation(AnnotatedBeanDefinition annotatedDef) {
+        AnnotationMetadata amd = annotatedDef.getMetadata();
+        Set<String> types = amd.getAnnotationTypes();
+        String beanName = null;
+        for (String type : types) {
+            AnnotationAttributes attributes = AnnotationConfigUtils.attributesFor(amd, type);
+            if (isStereotypeWithNameValue(type, amd.getMetaAnnotationTypes(type), attributes)) {
+                Object value = attributes.get("value");
+                if (value instanceof String) {
+                    String strVal = (String) value;
+                    if (StringUtils.hasLength(strVal)) {
+                        if (beanName != null && !strVal.equals(beanName)) {
+                            throw new IllegalStateException("Stereotype annotations suggest inconsistent " +
+                                    "component names: '" + beanName + "' versus '" + strVal + "'");
+                        }
+                        beanName = strVal;
+                    }
+                }
+            }
+        }
+        return beanName;
+    }
+
+    /**
+     * Check whether the given annotation is a stereotype that is allowed
+     * to suggest a component name through its annotation {@code value()}.
+     *
+     * @param annotationType      the name of the annotation class to check
+     * @param metaAnnotationTypes the names of meta-annotations on the given annotation
+     * @param attributes          the map of attributes for the given annotation
+     * @return whether the annotation qualifies as a stereotype with component name
+     */
+    protected boolean isStereotypeWithNameValue(String annotationType,
+                                                Set<String> metaAnnotationTypes, Map<String, Object> attributes) {
+
+        boolean isStereotype = annotationType.equals(COMPONENT_ANNOTATION_CLASSNAME) ||
+                (metaAnnotationTypes != null && metaAnnotationTypes.contains(COMPONENT_ANNOTATION_CLASSNAME)) ||
+                annotationType.equals("javax.annotation.ManagedBean") ||
+                annotationType.equals("javax.inject.Named");
+
+        return (isStereotype && attributes != null && attributes.containsKey("value"));
+    }
+
 }
